25cf4d689c1f52058c95e90f64092506ec9aae89
Limekee <esyropatov@gmail.com>
2026-06-11 23:44:33 +0500

    desc v4

--- backend/src/config.py
+++ backend/src/config.py
@@ -12,6 +12,8 @@ class Settings(BaseSettings):
     JWT_ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_SECONDS: int
 
+    PUBLIC_AI_URL:str
+
     @property
     def DB_URL(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
